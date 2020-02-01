298f95676053679e835084357e799207b7042d16
keithb <kieth.tienyou.bartlett@gmail.com>
2020-02-01 12:24:45 -0800

    toggle test thing for hack

---
+++ Assets/TestEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEntity : MonoBehaviour
+{
+    public bool isEnemy;
+    SpriteRenderer rend;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.color = isEnemy ? Color.red : Color.white; 
+    }
+}
