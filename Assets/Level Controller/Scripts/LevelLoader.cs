e795c103ed665edbedf25bafc46d40d5f7fcb2ce
Will Schultz <AzarathXorn@users.noreply.github.com>
2020-02-01 12:48:45 -0800

    Reorganize level controller and basic main menu

---
+++ Assets/Level Controller/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // load the next level in the build index
+    public void LoadNextLevel()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+
+    // load main menu
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
