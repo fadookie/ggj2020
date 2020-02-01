d833fc16c9bc2868cee1aafd659fe841ad6fe05b
Eliot Lash <fadookie@gmail.com>
2020-02-01 11:46:46 -0800

    Set up basic plumbing for EcsRx app

---
+++ Assets/Scripts/MainApplication.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using EcsRx.Unity;
+using EcsRx.Unity.Components;
+using UnityEngine;
+using UniRx;
+using Assets.EcsRx.Unity.Extensions;
+using EcsRx.Entities;
+using EcsRx.Extensions;
+using EcsRx.Unity.MonoBehaviours;
+using EcsRx.Unity.Systems;
+
+    public class MainApplication : EcsRxApplication
+    {
+        protected override void ApplicationStarting() {
+            Debug.Log("ApplicationStarting");
+            RegisterAllBoundSystems();
+        }
+
+        protected override void ApplicationStarted() {
+            Debug.Log("ApplicationStarted");
+            //var defaultPool = PoolManager.GetPool();
+            //var viewEntity = defaultPool.CreateEntity();
+            //viewEntity.AddComponent(new ViewComponent());
+            //viewEntity.AddComponent(new PlayerControlledComponent());
+            //viewEntity.AddComponent(new CameraFollowsComponent());
+        }
+
+
+        public void DestroyEntities(IEntity[] entities) {
+            foreach (var entity in entities) {
+                if (entity.HasComponent<ViewComponent>()) {
+                    var view = entity.GetComponent<ViewComponent>();
+                    if (view.View == null) {
+                        Debug.Log("MA.DestroyEntities skipping due to null view");
+                        continue;
+                    }
+                    if (view.DestroyWithView) {
+                        Debug.LogFormat("MA.DestroyEntities Destroying GameObject '{0}'", view.View.name);
+                        Destroy(view.View);
+                    } else {
+                        Debug.LogFormat("MA.DestroyEntities Destroying EntityView on '{0}'", view.View.name);
+                        Destroy(view.View.GetComponent<EntityView>());
+                    }
+                }
+            }
+            
+            PoolManager.GetPool().RemoveEntities(entities);
+            
+            foreach (var entity in entities) {
+               entity.Dispose(); 
+            }
+        }
+    }
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+//        var helloWorld = new Main.CustomApplication();
+//        helloWorld.ApplicationStarting();
+    }
+
+    // Update is called once per frame
+    void Update() {
+//        var transform = gameObject.transform.localPosition;
+//        transform.x += 1 * Time.deltaTime;
+//        gameObject.transform.localPosition = transform;
+    }
+}
