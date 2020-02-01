82af36297ca53abd7fbd49c5e909358f76a06b57
Brent Bartlett <brent.bartlett@gmail.com>
2020-02-01 10:54:20 -0800

    Not quite there.

---
+++ Assets/PlayerController/ShootArm.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArm : MonoBehaviour
+{
+    void Update()
+    {
+        
+        // Get Angle in Radians
+        float AngleRad = Mathf.Atan2(Input.mousePosition.y - transform.position.y, Input.mousePosition.x - transform.position.x);
+        // Get Angle in Degrees
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        // Rotate Object
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+}
---
+++ Assets/PlayerController/SimplePlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+
+            if (transform.localScale.y != 1)
+            {
+                transform.localScale *= -1;
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            if (transform.localScale.y != -1)
+            {
+                transform.localScale *= -1;
+            }
+        }
+
+
+        transform.position = pos;
+    }
+}
