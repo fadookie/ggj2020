b1847d54533aa777588404b03279c4469a2b63d0
Brent Bartlett <brent.bartlett@gmail.com>
2020-02-01 13:34:25 -0800

    Hackable menu load logic first pass.

--- Assets/Hackables/Hackable.cs
+++ Assets/Hackables/Hackable.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Hackable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isHacked;
+    SpriteRenderer rend;
+
+    Color _origColor;
     void Start()
     {
-        
+        rend = GetComponent<SpriteRenderer>();
+        _origColor = rend.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rend.color = isHacked ? Color.white : _origColor;
     }
 }
